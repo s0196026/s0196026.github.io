108ab91242c2dfae259464cefa8ba8731cc1eb92
s0196026 <elizavetagolodkova@gmail.com>
2025-10-13 14:46:11 +0300

    Update newjavascript.js

--- 5/newjavascript.js
+++ 5/newjavascript.js
@@ -1,12 +1,12 @@
 function count(){
-    let s = document.getElementsByName("select1")[0];
+    let s = document.getElementsByName("select1");
     let t = document.getElementsByName("text1");
     let r = document.getElementById("result");
     let price;
-    if (s.value === "o1") price = 10;
-    else if (s.value === "o2") price = 20;
-    else if (s.value === "o3") price = 100;
-    else if (s.value === "o4") price = 70;
+    if (s[0].value === "o1") price = 10;
+    else if (s[0].value === "o2") price = 20;
+    else if (s[0].value === "o3") price = 100;
+    else if (s[0].value === "o4") price = 70;
     else price = 0;
     r.innerHTML ="Стоимость заказа: "+t[0].value*price+" рублей";
     return false;
