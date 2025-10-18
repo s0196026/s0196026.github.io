c3f78837c8cb9127d11ae51df94fd11415029c87
s0196026 <elizavetagolodkova@gmail.com>
2025-10-18 15:51:15 +0300

    Update newjavascript.js

--- newjavascript.js
+++ newjavascript.js
@@ -1,13 +1,25 @@
-function count(){
-    let s = document.getElementsByName("select1")[0];
-    let t = document.getElementsByName("text1");
-    let r = document.getElementById("result");
-    let price;
-    if (s.value === "o1") price = 10;
-    else if (s.value === "o2") price = 20;
-    else if (s.value === "o3") price = 100;
-    else if (s.value === "o4") price = 70;
-    else price = 0;
-    r.innerHTML ="Стоимость заказа: "+t[0].value*price+" рублей";
-    return false;
-}
+    function count(event){
+        event.preventDefault();
+        let s = document.getElementsByName("select1")[0];
+        let t = document.getElementsByName("text1");
+        let r = document.getElementById("result");
+        if(isNaN(t[0].value)){
+            r.innerHTML ="Введите числовое значение!";
+            return false;
+        }
+        else{
+            let price;
+            if (s.value === "o1") price = 10;
+            else if (s.value === "o2") price = 20;
+            else if (s.value === "o3") price = 100;
+            else if (s.value === "o4") price = 70;
+            else price = 0;
+            r.innerHTML ="Стоимость заказа: "+t[0].value*price+" рублей";
+        }
+        return false;
+    }
+    
+    document.addEventListener('DOMContentLoaded', function () {
+        let b = document.getElementById("button_click");
+        b.addEventListener('click', count);
+    });
