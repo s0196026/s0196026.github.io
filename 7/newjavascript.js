783cec960e5ba21020768d81843e144fce75c8d7
s0196026 <elizavetagolodkova@gmail.com>
2025-11-16 16:23:03 +0300

    Update newjavascript.js

--- 7/newjavascript.js
+++ 7/newjavascript.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
     $('.cover').slick({
-        infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        responsive: [{
+                breakpoint: 719,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
     });
 });
