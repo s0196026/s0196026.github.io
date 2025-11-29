5a63a3258562968cbd1787c58248014f6936c9f3
s0196026 <elizavetagolodkova@gmail.com>
2025-11-29 16:17:27 +0300

    Update newjavascript.js

--- 8/newjavascript.js
+++ 8/newjavascript.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function closePopup() {
         popup.style.display = "none";
-        history.back();
+        //history.back();
     }
 
     function openPopup() {
@@ -90,4 +90,5 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
 
